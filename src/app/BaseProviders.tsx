e9c0a073af95121bf935fed0d4479a52f6079162
SecondNiceee <col1596321@gmail.com>
2025-01-21 18:20:28 +0300

    react-router-only

--- src/app/BaseProviders.tsx
+++ src/app/BaseProviders.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router';
 
 
 const BaseProviders = ({children} : {children : ReactNode}) => {
